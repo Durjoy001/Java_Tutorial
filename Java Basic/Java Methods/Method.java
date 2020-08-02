d8257967c7604fe36494b89481acbbd98b65fd7a
Durjoy Dey <durjoydey001@gmail.com>
2020-08-02 20:53:21 +0600

    Update Method.java

---
+++ Java Basic/Java Methods/Method.java
@@ -0,0 +1,19 @@
+package tutorial;
+
+/**
+ *
+ * @author Durjoy
+ */
+public class Tutorial {
+    
+    static void firstMethod() {
+        
+        System.out.println("I just got executed!");
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        firstMethod();
+    }
+}
