30e03f03d08a598fcbc4f2e596f4f0dda177b824
Durjoy Dey <durjoydey001@gmail.com>
2020-08-02 22:01:21 +0600

    Create BlockScope.java

---
+++ Java Basic/Java Scope/BlockScope.java
@@ -0,0 +1,29 @@
+package tutorial;
+
+/**
+ *
+ * @author Durjoy
+ */
+public class Tutorial {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    // Code here CANNOT use x
+
+      { // The block starts here
+
+      // Code here CANNOT use x
+
+      int x = 100;
+
+      // Code here CAN use x
+      
+      System.out.println(x);
+
+      } // The block ends here
+
+    // Code here CANNOT use x
+    
+    }
+}
