193175f6e7e91839b610a4e0ad27e26616d7da60
Durjoy Dey <durjoydey001@gmail.com>
2021-01-19 23:30:40 +0600

    Update Instance Method.java

--- Java OOPs Concepts/Java Object Class/Method/Instance Method.java
+++ Java OOPs Concepts/Java Object Class/Method/Instance Method.java
@@ -8,7 +8,7 @@ InstanceMethodExample obj = new InstanceMethodExample();
 System.out.println("The sum is: "+obj.add(12, 13));  
 }  
 int s;  
-//user-defined method because we have not used static keyword  
+//instance method because we have not used static keyword  
 public int add(int a, int b)  
 {  
 s = a+b;  
