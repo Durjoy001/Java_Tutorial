6edf77eac1c61fc6cea47918e9bce37dab8ad1c0
Durjoy Dey <durjoydey001@gmail.com>
2021-01-21 18:13:52 +0600

    Create Hierarchical Inheritance Example.java

---
+++ Java OOPs Concepts/Inheritance/Hierarchical Inheritance Example.java
@@ -0,0 +1,27 @@
+package objectandclass;
+/**
+ *
+ * @author Durjoy
+ */ 
+class Animal{
+    void eat(){
+        System.out.println("Eating...");
+    }
+}
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Barking...");
+    }
+}
+class Cat extends Animal{
+    void meow(){
+        System.out.println("meowing...");
+    }
+}
+public class ObjectAndClass {
+    public static void main(String[] args) { 
+      Cat c = new Cat();
+      c.meow();
+      c.eat();
+    }
+}
